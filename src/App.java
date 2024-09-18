6929c65bdf73aadc23e7abc27bf5ee30b5c06d47
Jaasiel Silva <jasiel1@outlook.com>
2024-09-17 23:29:51 -0300

    Commit inicial do projeto

---
+++ src/App.java
@@ -0,0 +1,37 @@
+import java.util.Scanner;
+
+public class Contador {
+    public static void main(String[] args) {
+        Scanner scanner = new Scanner(System.in);
+
+        try {
+            // Recebendo os parâmetros via terminal
+            System.out.println("Digite o primeiro número:");
+            int primeiroParametro = scanner.nextInt();
+
+            System.out.println("Digite o segundo número:");
+            int segundoParametro = scanner.nextInt();
+
+            // Validando os parâmetros
+            if (primeiroParametro > segundoParametro) {
+                throw new ParametrosInvalidosException("O segundo parâmetro deve ser maior que o primeiro");
+            }
+
+            // Realizando as iterações e imprimindo os números
+            contar(primeiroParametro, segundoParametro);
+
+        } catch (ParametrosInvalidosException e) {
+            System.out.println(e.getMessage());
+        } finally {
+            scanner.close();
+        }
+    }
+
+    // Função para realizar as interações e imprimir os números
+    static void contar(int primeiroParametro, int segundoParametro) {
+        int quantidade = segundoParametro - primeiroParametro;
+        for (int i = 1; i <= quantidade; i++) {
+            System.out.println("Imprimindo o número " + i);
+        }
+    }
+}
